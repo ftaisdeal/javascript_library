9e853bca8de147150889787076e305582d18c359
Firinn Taisdeal <firinn@taisdeal.com>
2020-09-11 13:11:19 -0700

    Add example of detecting duplicate characters in a string

---
+++ exercises/detect_dupllcate_char.js
@@ -0,0 +1,30 @@
+/* For any given string, detect whether the string contains any duplicate characters.
+If any duplicate character is detected, return false.
+If no dupiicate characters are detected, return the string. */
+
+let detect_duplicate = function (string) {
+
+  let test_array = [];
+
+  for (i = 0; i < string.length; i++) {
+
+    if (test_array.indexOf(string[i]) == -1) {
+
+      test_array.push(string[i]);
+
+    } else {
+
+      console.log('duplicate');
+      return false;
+
+    }
+
+  }
+
+  console.log(string);
+  return string;
+
+}
+
+detect_duplicate("abc");
+detect_duplicate("abcc");
