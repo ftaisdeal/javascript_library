c7f34f85a1b17708bf507e88223f2c01ce781df3
Firinn Taisdeal <firinn@taisdeal.com>
2020-09-16 16:55:36 -0700

    Add PDF of notes on JavaScript, new directory about functions

--- switch.js
+++ switch.js
@@ -32,11 +32,11 @@ let b = 0;
 
 switch (+a) {
   case b + 1:
-    alert("this runs, because +a is 1, exactly equals b+1");
+    console.log("this runs, because +a is 1, exactly equals b+1");
     break;
 
   default:
-    alert("this doesn't run");
+    console.log("this doesn't run");
 }
 
 
@@ -48,20 +48,49 @@ let a = 3;
 
 switch (a) {
   case 4:
-    alert('Right!');
+    console.log('Right!');
     break;
 
   case 3: // (*) grouped two cases
   case 5:
-    alert('Wrong!');
-    alert("Why don't you take a math class?");
+    console.log('Wrong!');
+    console.log("Why don't you take a math class?");
     break;
 
   default:
-    alert('The result is strange. Really.');
+    console.log('The result is strange. Really.');
 }
 
 // Now both 3 and 5 show the same message.
 
 // The ability to “group” cases is a side-effect of how switch/case works without break.
 // Here the execution of case 3 starts from the line (*) and goes through case 5, because there’s no break.
+
+
+// Type matters
+// Let’s emphasize that the equality check is always strict. The values must be of the same type to match.
+// For example, let’s consider the code:
+
+let arg = "3";
+
+switch (arg) {
+  case '0':
+  case '1':
+    console.log('One or zero');
+    break;
+
+  case '2':
+    console.log('Two');
+    break;
+
+  case 3:
+    console.log('Never executes!');
+    break;
+  default:
+    console.log('An unknown value');
+}
+
+// For 0, 1, the first alert runs.
+// For 2 the second alert runs.
+// But for 3, the result of the prompt is a string "3", which is not strictly equal === to the number 3.
+// So we’ve got a dead code in case 3! The default variant will execute.
