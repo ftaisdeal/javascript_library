41911fda6f52d5ade99810cfdafbe6162275a987
Firinn Taisdeal <firinn@taisdeal.com>
2020-11-08 19:29:26 -0800

    Miinor edits

--- arrow_functions/arrow_functions-1.js
+++ arrow_functions/arrow_functions-1.js
@@ -1,5 +1,7 @@
 /*
-An arrow function expression is a syntactically compact alternative to a regular function expression, although without its own bindings to the this, arguments, super, or new.target keywords. Arrow function expressions are ill suited as methods, and they cannot be used as constructors.
+An arrow function expression is a syntactically compact alternative to a regular function expression,
+although without its own bindings to the this, arguments, super, or new.target keywords.
+Arrow function expressions are ill suited as methods, and they cannot be used as constructors.
 */
 
 // Example 1
