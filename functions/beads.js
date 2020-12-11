a493961ef60ee5a2eeed54ca5fd6af968e522968
Firinn Taisdeal <firinn@taisdeal.com>
2020-12-10 16:16:38 -0800

    Add challenge for string of beads

---
+++ functions/beads.js
@@ -0,0 +1,44 @@
+/**
+ * As part of an automated bracelet assembly system we need to generate
+ * the pattern of colored beads used to construct a bracelet. This method
+ * is responsible for taking in the available colors and final length
+ * of the bracelet and returning the final pattern to be constructed.
+ * There are two restrictions in the final pattern:
+ *   1) No two adjacent beads may be the same color.
+ *   2) No pattern of three beads may be repeated anywhere in the bracelet.
+ *      For example (Red, Yellow, Red, Yellow, Red) repeats the pattern
+ *      (Red, Yellow, Red) twice and would be a violation of this rule.
+ *      (Red, Yellow, Blue, Green, Red, Yellow, Blue) would also violate this rule.
+ *
+ * @param colors - An array of unique Strings where each string represents a bead color.
+ * @param size - The length of the bracelet to be constructed.
+ * @return - An array of colors indicating the final order of beads that should be used to construct a bracelet.
+ */
+const generateBeads = function (colors /* array */, size /* number */) {
+
+  beads = [];
+  count = 0;
+
+  for (let i = 0; i < size; i++) {
+
+    if (count >= colors.length) { // Reset to beginning of colors array
+      count = 0;
+    }
+
+    if ((i != 1) && (colors[count] != beads[i - 1])) { // Prevent repetition of adjacent color
+      beads.push(colors[count]);
+    }
+
+    if (1) { // Prevent repetition of three colors
+
+    }
+
+    count++;
+
+  }
+
+  return beads;
+
+};
+
+console.log(generateBeads(["Red", "Red", "Red", "Green", "Yellow", "Blue", "Blue", "Black"], 10));
--- functions/functions1.js
+++ functions/functions1.js
@@ -4,4 +4,5 @@ function defaultParameters(text, code = '[no code included]') {
   console.log('The text: ' + text + '\nThe code: ' + code)
 }
 
+defaultParameters('some text', 'code');
 defaultParameters('some text');
