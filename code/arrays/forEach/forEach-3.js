b2743d027571e7cfad8f700273f54ff87988de22
Firinn Taisdeal <firinn@taisdeal.com>
2021-03-12 13:57:53 -0500

    Finish code for generating grid

--- code/arrays/forEach/forEach-3.js
+++ code/arrays/forEach/forEach-3.js
@@ -1,30 +1,48 @@
 // Example using a function passed into forEach
 
 const arr = [
-  0, 1, 2, 3, 4, 5, 6, 7, 8,
-  0, 1, 2, 3, 4, 5, 6, 7, 8,
-  0, 1, 2, 3, 4, 5, 6, 7, 8,
-  0, 1, 2, 3, 4, 5, 6, 7, 8,
-  0, 1, 2, 3, 4, 5, 6, 7, 8,
-  0, 1, 2, 3, 4, 5, 6, 7, 8,
-  0, 1, 2, 3, 4, 5, 6, 7, 8,
-  0, 1, 2, 3, 4, 5, 6, 7, 8,
-  0, 1, 2, 3, 4, 5, 6, 7, 8,
+  1, null, null, null, null, null, null, null, null,
+  null, null, null, null, null, null, null, null, null,
+  null, null, 7, null, null, null, null, null, null,
+  null, null, null, null, null, null, null, null, null,
+  null, null, null, null, 3, null, null, null, null,
+  null, null, null, null, null, null, null, null, null,
+  null, null, null, 9, null, null, null, null, null,
+  null, null, null, null, null, null, 4, null, null,
+  null, null, null, null, null, null, null, null, null
+
 ];
 
 let grid = "<div>";
 
 function formatGrid(arr) {
+  let entry;
+  let disabled;
+  let group_row = "";
   for (let i = 0; i < arr.length; i++) {
+    if ((i % 27 === 0) && (i != 0)) {
+      group_row = " group_row";
+    } else if (i % 27 === 9) {
+      group_row = "";
+    }
+    if (arr[i] != null) {
+      entry = arr[i];
+      disabled = " disabled";
+    } else {
+      entry = "0";
+      disabled = "";
+    }
     if (i % 9 === 0) {
       grid = `${grid}`;
     }
-    if (i % 3 === 0) {
-      grid = `${grid}<button class="group_col">${arr[i]}</button>`;
+    if ((i % 9 === 2) || (i % 9 === 5)) {
+      grid = `${grid}<button class="square group_col${group_row}${disabled}"${disabled}>${entry}</button>`;
     } else {
-      grid = `${grid}<button>${arr[i]}</button>`;
+      grid = `${grid}<button class="square${group_row}${disabled}"${disabled}>${entry}</button>`;
     }
-    if (i % 9 === 8) {
+    if ((i % 9 === 8) && (i === arr.length - 1)) {
+      grid = `${grid}</div>`;
+    } else if (i % 9 === 8) {
       grid = `${grid}</div>\n<div>`;
     }
   }
