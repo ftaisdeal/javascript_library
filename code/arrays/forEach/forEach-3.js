dabcc7df063d005a84bc1c31d4b475df24f4c905
Firinn Taisdeal <firinn@taisdeal.com>
2021-03-12 11:25:30 -0500

    Refine formatGrid function

--- code/arrays/forEach/forEach-3.js
+++ code/arrays/forEach/forEach-3.js
@@ -1,25 +1,36 @@
 // Example using a function passed into forEach
 
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9,
-  1, 2, 3, 4, 5, 6, 7, 8, 9
+const arr = [
+  0, 1, 2, 3, 4, 5, 6, 7, 8,
+  0, 1, 2, 3, 4, 5, 6, 7, 8,
+  0, 1, 2, 3, 4, 5, 6, 7, 8,
+  0, 1, 2, 3, 4, 5, 6, 7, 8,
+  0, 1, 2, 3, 4, 5, 6, 7, 8,
+  0, 1, 2, 3, 4, 5, 6, 7, 8,
+  0, 1, 2, 3, 4, 5, 6, 7, 8,
+  0, 1, 2, 3, 4, 5, 6, 7, 8,
+  0, 1, 2, 3, 4, 5, 6, 7, 8,
 ];
 
 let grid = "<div>";
 
-function formatGrid(e) {
-  grid = grid + `<button>${e}</button>`;
-  if (e % 9 === 0) {
-    grid = `${grid}</div>\n`;
+function formatGrid(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (i % 9 === 0) {
+      grid = `${grid}`;
+    }
+    if (i % 3 === 0) {
+      grid = `${grid}<button class="group_col">${arr[i]}</button>`;
+    } else {
+      grid = `${grid}<button>${arr[i]}</button>`;
+    }
+    if (i % 9 === 8) {
+      grid = `${grid}</div>\n<div>`;
+    }
   }
-  if (e % 3 == 0) {
-    grid = grid + `<button className="group">${e}</button>`;
-  } else {
-    grid = grid + `<button>${e}</button>`;
-  }
+  return grid;
 }
 
-arr.forEach(formatGrid);
-
-grid = grid + "</div>";
+formatGrid(arr);
 
 console.log(grid);
