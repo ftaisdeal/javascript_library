cf5afaf5542c811193fd397d82062ede46c692fb
Firinn Taisdeal <firinn@taisdeal.com>
2021-03-12 10:36:17 -0500

    Add new example of forEach

--- code/arrays/forEach/forEach-3.js
+++ code/arrays/forEach/forEach-3.js
@@ -1,11 +1,25 @@
 // Example using a function passed into forEach
 
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9,
+  1, 2, 3, 4, 5, 6, 7, 8, 9
+];
+
+let grid = "<div>";
 
 function formatGrid(e) {
-  if (e % 3 === 0) {
-
+  grid = grid + `<button>${e}</button>`;
+  if (e % 9 === 0) {
+    grid = `${grid}</div>\n`;
+  }
+  if (e % 3 == 0) {
+    grid = grid + `<button className="group">${e}</button>`;
+  } else {
+    grid = grid + `<button>${e}</button>`;
   }
 }
 
-count.forEach(formatGrid);
+arr.forEach(formatGrid);
+
+grid = grid + "</div>";
+
+console.log(grid);
