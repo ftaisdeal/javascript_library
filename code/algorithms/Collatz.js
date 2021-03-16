ecc0ac6dd37c7029bd7b639d7f532ab860e8472a
Firinn Taisdeal <firinn@taisdeal.com>
2021-03-16 10:02:54 -0400

    Amend Collatz code

--- code/algorithms/Collatz.js
+++ code/algorithms/Collatz.js
@@ -1,6 +1,5 @@
 function callCollatz(x) {
   let count = 0;
-
   function Collatz(x) {
     if (x % 2 === 0) {
       x = x / 2;
@@ -9,12 +8,11 @@ function callCollatz(x) {
       x = x * 3 + 1;
       count++;
     }
-    if (x > 1) {
+    if (x != 1) {
       Collatz(x);
     }
     return count;
   }
-
   return Collatz(x);
 }
 
@@ -22,12 +20,10 @@ let longest = 0;
 let winner;
 
 for (i = 1000000; i > 2; i--) {
-
   if (callCollatz(i) > longest) {
     longest = callCollatz(i);
     winner = i;
   }
-
 }
 
 console.log(`${winner} is the winner, with a string of numbers of length ${longest}.`);
