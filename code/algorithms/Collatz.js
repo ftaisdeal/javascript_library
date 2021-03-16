33ac631de9bafd17c98fab21860c0bbe32270d72
Firinn Taisdeal <firinn@taisdeal.com>
2021-03-15 21:20:55 -0400

    Collatz

--- code/algorithms/Collatz.js
+++ code/algorithms/Collatz.js
@@ -1,13 +1,40 @@
-function Collatz(num) {
+let count = 0;
 
-  let result;
+function Collatz(x) {
 
-  if (num % 2 === 0) {
-    result = num / 2;
+  if (x % 2 === 0) {
+    x = x / 2;
+    count++;
   } else {
-    result = num * 3 + 1;
+    x = x * 3 + 1;
+    count++;
   }
-  return "x";
+
+  if (x > 1) {
+    Collatz(x);
+  }
+
+  //console.log(x);
+  return count;
 }
 
-console.log(1);
+let longest = 0;
+
+for (i = 13; i > 1; i--) {
+  console.log(Collatz(i));
+
+  temp = Collatz(i);
+
+  if (temp > longest) {
+    longest = i;
+  }
+
+}
+
+console.log(longest);
+
+
+
+// Get the function to spit out the iterative results
+// Count how many results and record the number used
+// Build a for loop that counts down from 1 million and records the number with the longest string of numbers
