c9b2f6b0f33ba518fd76084f3a85d6edcf52a583
Firinn Taisdeal <firinn@taisdeal.com>
2022-04-22 09:08:40 -0400

    improve "int or string" example

--- code/int_or_string.js
+++ code/int_or_string.js
@@ -1,15 +1,17 @@
-// Write a function that returns 'int' if the input is an integer, and 'str' if the input is a string.
+/*
+Write a function that returns 'int' if the input is an integer, 
+and 'str' if the input is a string.
+*/
 
-// Solutions
-
+// Solution 1
 const intOrString1 = (param) => typeof param === 'string' ? 'str' : 'int';
 
-
+// Solution 2
 function intOrString2(param) {
-  return param * 0 === 0 ? 'int' : 'str'
+  return param * 0 === 0 ? 'int' : 'str';
 }
 
-
+// Solution 3
 function intOrString3(param) {
   return typeof param == 'number' ? 'int' : 'str';
 }
