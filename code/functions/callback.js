56f113b9bc0a063a78747f0a0bdeb57e5cd654c0
Firinn Taisdeal <firinn@taisdeal.com>
2022-04-22 08:57:46 -0400

    edit example of callback

--- code/functions/callback.js
+++ code/functions/callback.js
@@ -1,13 +1,16 @@
-function Test(callback) {
+/*
+A callback is a function entered as an input to a function.
+The callback can then be used within the outer function.
+*/
 
+function test(callback) {
   result = callback(2);
-
 }
 
 function callback(int) {
   return int;
 }
 
-Test(callback);
+test(callback);
 
 console.log(result);
